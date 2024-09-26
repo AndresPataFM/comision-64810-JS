53c5210abba46ba65302f0e0b844956ff3cd4e4f
AndresPataFM <apfm21@Gmail.com>
2024-09-25 22:38:15 -0300

    primer commit

---
+++ clase-1/en-vivo/clase1-vivo.js
@@ -0,0 +1,44 @@
+let frase = 'Andrés dijo: "Buenas noches()#2143;0-" '
+
+const nroFavorito = 4;
+
+/* const nombre = prompt("Ingrese su nombre")
+
+console.log(`El usuario tiene de nombre ${nombre}, su nro favorito es ${nroFavorito} y ese nro multiplicado pro cinco es ${nroFavorito*5}`)
+
+console.log(`El usuario tiene de nombre `+ nombre + `, su nro favorito es ${nroFavorito} y ese nro multiplicado pro cinco es ${nroFavorito*5}`) */
+
+const ejemplo = "asd ${}";
+const ejemplo2 = 'asd ${}';
+
+let usuarioRegistrado = false;
+
+// aca sucede algo que registra al usuario
+
+usuarioRegistrado = true;
+
+const algo = "47"
+
+console.log( "Tipo de dato de algo: ",typeof algo, algo)
+
+const yoQueSe = 47
+
+console.log( "Tipo de dato de yoQueSe: ",typeof yoQueSe, yoQueSe)
+
+console.log("4" + 7)
+
+console.log("asd" + "algo")
+
+console.log("asd" - "a")
+console.log("4" - 7)
+
+console.log(Number(algo) +3)
+
+const peticion = prompt("numero")
+
+console.log(peticion)
+console.log("Tipo de dato de petición ", typeof peticion)
+
+const confirmar = confirm("Si o no")
+
+console.log(confirmar)
---
+++ clase-1/en-vivo/ejemplo.js
@@ -0,0 +1,75 @@
+// alert("Conectado con exito")
+
+/* 
+
+
+
+*/
+
+// Variables
+
+let nombre = "Andrés";
+
+console.log(nombre);
+
+/* let datoIngresado = prompt("Ingrese un número");
+
+console.log(datoIngresado); */
+
+let saludo = "Hola a todos";
+
+console.log(saludo)
+
+saludo = "buenas, como están";
+
+/* console.log(saludo)
+
+let contador = 0;
+console.log(contador)
+
+contador = contador +1
+console.log(contador)
+
+contador = contador +1
+console.log(contador)
+
+contador = contador +1
+console.log(contador)
+
+contador = contador +1
+console.log(contador)
+
+contador = contador +1
+console.log(contador)
+
+contador = contador +1
+console.log(contador) */
+
+const dni = 45646456456
+
+// dni = 0
+
+let numero = 4+7;
+
+console.log(numero);
+
+const numerito = 3*2;
+
+console.log(numerito);
+
+const palabras = prompt("Diga unas palabras");
+
+// console.log(palabras)
+
+
+console.log(frase)
+
+const frase =  "El usuario " + nombre + " dijo lo siguiente: " + palabras;
+
+
+// Primer pecado capital de var
+var soyFeo = "si lo soy";
+
+// let numero = 7 // No permite porque ya hay una variable con ese nombre
+
+var soyFeo = 7;
