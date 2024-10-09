d968f48709c0359100e8623413c4ffcfc08b0569
AndresPataFM <apfm21@Gmail.com>
2024-10-09 20:30:14 -0300

    inicio clase 3

---
+++ clase-3/micro3/micro3.js
@@ -0,0 +1,30 @@
+/* 
+La semana estudiaste la sintaxis para la creación de funciones, aplicando los conocimientos adquiridos completa la función.
+Una vez completada tomala como base para crear una función flecha pero pasando los valores a sumar como parámetros en la función sumar. 
+*/
+
+/* 
+________ sumar() {
+  let numero1 = 5;
+  let ________ = numero1 + 5;
+
+  ________ resultado;
+}
+
+________ (sumar());
+*/
+
+
+
+// console.log(sumar());
+
+
+// flecha indica funcion
+// que no halla llaves después de la flecha indica return implicito
+
+/* const sumar = ()=>{
+  let numero1 = 5;
+  let resultado = numero1 + 5;
+  return resultado;
+} */
+
