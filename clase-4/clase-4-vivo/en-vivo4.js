8534f53a0478496a80fe5d6de3e9cf1f21f7ebfb
AndresPataFM <apfm21@Gmail.com>
2024-10-16 22:21:02 -0300

    final clase 4

--- clase-4/clase-4-vivo/en-vivo4.js
+++ clase-4/clase-4-vivo/en-vivo4.js
@@ -131,3 +131,96 @@ agregarNumeros(2)
 agregarNumeros("hola")
 
 console.log(numeros) */
+// nombres.unshift("Alejandro")
+// console.log(nombres)
+
+// console.log(nombres.sort())
+// nombres.reverse()
+
+// nombres.sort().reverse()
+
+/* function aZNombres(){
+  nombres.sort()
+  console.table(nombres)
+}
+
+function zANombres(){
+  nombres.sort().reverse()
+  console.table(nombres)
+} */
+
+
+
+// console.log(nombres)
+
+/* const numeros = [2, 13, 131, 1000, 4]
+
+numeros.sort()
+
+console.log(numeros) */
+
+const pacientes = []
+
+function mostrarPacientes(){
+  let listaPacientes = "Los turnos son:"
+  let i = 0
+  for(paciente of pacientes){
+    i++
+    listaPacientes += `\n ${i} - ${paciente}`
+  }
+  alert(listaPacientes)
+}
+
+function agregarPaciente(){
+  let nombre = prompt("Ingrese el nombre del paciente que llego recien:")
+  pacientes.push(nombre)
+  mostrarPacientes()
+}
+
+function dejarPasarPaciente(){
+  let primerPaciente = pacientes.shift()
+  if(pacientes.length === 0){
+    alert(`Por favor, que pase el paciente ${primerPaciente}`)
+  } else {
+    alert(`Por favor, que pase el paciente ${primerPaciente} y que se valla preparando ${pacientes[0]}`)
+    mostrarPacientes
+  }
+}
+
+const encontrarPaciente = (nombre)=>{
+  const indicePaciente = pacientes.indexOf(nombre)
+  alert(`Encontramos su nombre en el orden ${indicePaciente+1} en la lista`)
+}
+
+function seleccionarOpcion(opcion){
+  switch(opcion){
+    case 1: 
+      agregarPaciente()
+      break;
+    case 2:
+      dejarPasarPaciente();
+      break;
+    case 3:
+      const nombrePaciente = prompt("Ingrese el nombre del paciente (cuidado con mayúsculas, tildes y espacio)")
+      encontrarPaciente(nombrePaciente);
+      break
+    case 4:
+      alert("No se realzio nada")
+    default:
+      alert("Se ingreso un dato no válido")
+  }
+}
+
+
+function app(){
+  let loop = true
+  alert("Buenos días")
+  while(loop){
+    let opcion = parseInt(prompt("Operaciones a realizar: \n 1 - Agregar Paciente \n 2 - Dejar Pasar Paciente \n 3- Buscar en la lista al paciente \n 4- Nada"))
+    seleccionarOpcion(opcion)
+    loop = confirm("¿Desea seguir realizando operaciones?")
+  }
+  alert("Que tenga un buen día")
+}
+
+app()
