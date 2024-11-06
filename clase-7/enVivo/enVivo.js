fb50852216762fc014eea6160bdae69cc2c2790f
AndresPataFM <apfm21@Gmail.com>
2024-11-06 20:27:22 -0300

    inicio clase 7

---
+++ clase-7/enVivo/enVivo.js
@@ -0,0 +1,59 @@
+// Procedimiento
+// 🔶 Encontrar Nodos 🔶
+// 🔶 Modificar Nodos 🔶
+
+// o
+
+// 🔶 Crear Nodos 🔶
+// 🔶 Modificar Nodos 🔶
+// 🔶 Agregarlo al DOM 🔶
+
+
+// 🔶 Encontrar Nodos 🔶
+// 🔹 document.getElementById 🔹
+// array.find
+
+
+
+// // 🔹 document.getElementsByClassName 🔹
+// // array vivo => Nunca lo modifiquen mientras lo recorren
+// // array.filter
+
+
+// // puede buscar combinación de clases
+
+
+// // // 🔹 document.getElementsByTagName 🔹
+// // Es similar a filter con un array vivo
+const divs = document.getElementsByTagName("div")
+
+
+// // // 🔹 document.querySelector 🔹
+// // como find, devuelve el primero que cumpla
+
+
+// // // 🔹 document.querySelectorAll 🔹
+
+
+// // 🔶 Modificar Nodos 🔶
+
+
+
+
+// // 🔶 Crear Nodos 🔶
+const listaDeberes = document.getElementById("listaDeberes")
+
+// metodo bruto
+
+// parte a parte
+
+// Mas usados
+// classlist
+// innerHTML
+// innerText
+// id
+
+
+// 🔶 Eventos 🔶
+
+// ============
