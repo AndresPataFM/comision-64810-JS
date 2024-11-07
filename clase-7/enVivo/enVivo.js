c8f566ab94b7c982cfe6294c42345ecce3a56b26
AndresPataFM <apfm21@Gmail.com>
2024-11-06 22:36:24 -0300

    final clase 7

--- clase-7/enVivo/enVivo.js
+++ clase-7/enVivo/enVivo.js
@@ -12,13 +12,23 @@
 // 🔶 Encontrar Nodos 🔶
 // 🔹 document.getElementById 🔹
 // array.find
+const encontrame = document.getElementById("encontrame")
 
+// console.log(encontrame)
+// console.dir(encontrame)
 
+const menu = document.getElementById("menu")
+// console.log(menu)
+
+// const liHTML = menu.getElementById("liHTML")
+
+// console.log(liHTML)
 
 // // 🔹 document.getElementsByClassName 🔹
 // // array vivo => Nunca lo modifiquen mientras lo recorren
 // // array.filter
-
+const rojos = document.getElementsByClassName("rojo")
+// console.log(rojos)
 
 // // puede buscar combinación de clases
 
@@ -26,34 +36,92 @@
 // // // 🔹 document.getElementsByTagName 🔹
 // // Es similar a filter con un array vivo
 const divs = document.getElementsByTagName("div")
+// console.log(divs)
 
 
 // // // 🔹 document.querySelector 🔹
 // // como find, devuelve el primero que cumpla
+const magenta = document.querySelector("#encontrame.magenta")
+const confiable = document.querySelector("#menu")
+// console.log(magenta)
+// console.log(confiable)
 
+const primerRojo = document.querySelector(".rojo")
+const primerP = document.querySelector("p")
+// console.log(primerP)
 
 // // // 🔹 document.querySelectorAll 🔹
+// // similar a filter
+
+const todosLi = document.querySelectorAll("li")
+// console.log(todosLi)
 
 
 // // 🔶 Modificar Nodos 🔶
+console.log(encontrame.innerHTML)
+console.log(encontrame.innerText)
+console.log(encontrame.textContent)
 
+// encontrame.textContent = "Te encontre"
+// encontrame.textContent = "Nuevamente"
 
+// encontrame.innerHTML = "<button>Alo</button>"
 
+console.log(encontrame.className)
+// encontrame.className = "rojo cursiva carta"
+console.log(encontrame.classList)
 
-// // 🔶 Crear Nodos 🔶
-const listaDeberes = document.getElementById("listaDeberes")
-
-// metodo bruto
-
-// parte a parte
+encontrame.classList.toggle("verde")
 
 // Mas usados
 // classlist
 // innerHTML
 // innerText
+// textContent
 // id
 
+// // 🔶 Crear Nodos 🔶
+const deberes = ["armar cama", "limpiar", "compras", "terminar proyecto"]
+
+const listaDeberes = document.getElementById("listaDeberes")
+
+// metodo bruto
+// function mostrarDeberes(){
+//   listaDeberes.innerHTML = ""
+//   deberes.forEach(deber=>{
+//     listaDeberes.innerHTML += `<li>${deber}</li>`
+//   })
+//   console.log(listaDeberes.innerHTML)
+// }
+
+// parte a parte
+function mostrarDeberes(){
+  listaDeberes.innerHTML = ""
+  deberes.forEach(deber=>{
+    const li = document.createElement("li")
+    li.textContent = deber
+    listaDeberes.appendChild(li)
+  })
+}
+
+const h2 = document.createElement("h2")
+h2.textContent = "alo"
+document.body.appendChild(h2)
 
 // 🔶 Eventos 🔶
+
+const trabajador = document.getElementById("eventos")
+
+// trabajador.onclick = ()=>{alert("hola")}
+// trabajador.onclick = alert("hola")
+
+// trabajador.addEventListener("click",function(){
+//   console.log("no me toques")
+// })
+// trabajador.addEventListener("click",function(){
+//   console.log("por favor")
+// })
+// trabajador.onclick = ()=>{console.log("no me toques")}
+// trabajador.onclick = ()=>{console.log("por favor")}
 
 // ============
--- clase-7/enVivo/eventosVivo.js
+++ clase-7/enVivo/eventosVivo.js
@@ -4,6 +4,55 @@
 
 const botoncito = document.getElementById("botoncito")
 const texto = document.getElementById("texto")
+const formulario = document.getElementById("formulario")
+const keylogger = document.getElementById("keylogger")
+const mensaje = document.getElementById("mensaje")
+const respuesta = document.getElementById("respuesta")
+
+botoncito.addEventListener("click", ()=>{
+  mensaje.innerText = "No toques al botón, no le gusta"
+})
+
+// formulario.addEventListener("submit",(evento)=>{
+//   evento.preventDefault()
+//   respuesta.innerHTML = ""
+//   respuesta.textContent = "Usted ingreso: " + evento.target[0].value
+// })
+
+keylogger.addEventListener("keypress", ()=>{
+  console.log("keypress", keylogger.value)
+})
+keylogger.addEventListener("keydown", ()=>{
+  console.log("keydown", keylogger.value)
+})
+keylogger.addEventListener("keyup", ()=>{
+  console.log("keyup", keylogger.value)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // console.log(botoncito)
 
@@ -34,13 +83,13 @@ const texto = document.getElementById("texto")
 // botoncito.addEventListener("click", alert("no me toques"))
 
 
-const formulario = document.getElementById("formulario")
+/* const formulario = document.getElementById("formulario")
 const keylogger = document.getElementById("keylogger")
 const mensaje = document.getElementById("mensaje")
 
 
 console.log(formulario)
-console.log(keylogger)
+console.log(keylogger) */
 
 // keylogger.addEventListener("change", (event)=>{
 //   console.log(event.target.value)
@@ -57,6 +106,15 @@ console.log(keylogger)
 //   console.log(e.target[0].value)
 // })
 
-keylogger.addEventListener("keydown", (e)=>{
+/* keylogger.addEventListener("keydown", (e)=>{
   console.log(e.target.value)
 })
+keylogger.addEventListener("keyup", (e)=>{
+  console.log(e.target.value)
+})
+
+formulario.addEventListener("submit", (e)=>{
+  e.preventDefault()
+  console.log(e)
+  console.dir(e.target[0].value)
+}) */
